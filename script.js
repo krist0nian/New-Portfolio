36fc7e9cf2dad819e719828024daf8ea02bd28de
krist0nian <krist0ian@yahoo.com>
2026-07-12 12:21:51 -0400

    Completed Modal Contact Form

--- script.js
+++ script.js
@@ -2,3 +2,37 @@ function toggleDarkMode() {
     document.querySelector('body').classList.toggle("dark-theme")
     console.log('clicked');
 }
+
+let isModalOpen = false;
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            'service_ezjxryl',
+            'template_hjdxf61',
+            event.target,
+            'qSiVBpC_Qre_9rF7W'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at krist0nian@yahoo.com"
+            );
+        })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
